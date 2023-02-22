176c814dd1a9ebacfa3ac1600fced87ff8cf6978
Miltonnnotforyou <miltonnotforyou@yahoo.com.br>
2023-02-22 08:12:35 -0300

    Adicionando arquivos do curso

---
+++ src/Gerente.java
@@ -0,0 +1,23 @@
+//gerente herda da class funcionario
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+
+    public boolean autentica(int senha) {
+        if (this.senha == senha) {
+            return true;
+        } else {
+            return false;
+        }
+
+
+    }
+
+
+    //public double getBonificacao() {
+      //  return this.salario;
+    //}
+}
---
+++ src/TesteGerente.java
@@ -0,0 +1,17 @@
+public class TesteGerente {
+    public static void main(String[] args) {
+        Gerente g1 = new Gerente();
+        g1.setNome("Marcos");
+        g1.setCpf("225.444.888-89");
+        g1.setSalario(5000.0);
+
+        System.out.println(g1.getNome());
+        System.out.println(g1.getCpf());
+        System.out.println(g1.getSalario());
+
+        g1.setSenha(252525);
+        boolean autenticou = g1.autentica(252525);
+
+        System.out.println(autenticou);
+    }
+}
